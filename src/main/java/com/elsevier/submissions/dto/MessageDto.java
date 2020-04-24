341074e8a3307573df3579cf48ec9d588de79498
sharm40 <g.sharma@elsevier.com>
2020-04-24 09:38:51 -0400

    Final Changelist : First commit for dto

---
+++ src/main/java/com/elsevier/submissions/dto/MessageDto.java
@@ -0,0 +1,14 @@
+package com.elsevier.submissions.dto;
+
+public class MessageDto {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
